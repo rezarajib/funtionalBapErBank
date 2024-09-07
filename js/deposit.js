2064a82fdb706c8d227f6d93ca8d855ee247201b
Rajib Reza <rajibreza64@gmail.com>
2024-09-07 15:14:35 +0600

    this is modify deposit str to num

--- js/deposit.js
+++ js/deposit.js
@@ -1,9 +1,11 @@
-function getInputFieldValueById(inputId){
-    const inputFiled = document.getElementById('depositField');
-    const inputFieldValueString = inputFiled.value;
-    const inputFieldStringToNum = parseFloat(inputFieldValueString);
-    return inputFieldStringToNum;
+//  add fuction all deposti field and withdraw field same so use for the function work easy
+function inputFieldValueById(inputId){
+    const inputField = document.getElementById('depositField');
+    const inputFieldAddValue = inputField.value;
+    const inputFieldStrToNum = parseFloat(inputFieldAddValue);
+    return inputFieldStrToNum;
 }
+//  add event for deposit btn
 document.getElementById('depositBtn').addEventListener('click',function(){
-    // console.log("added new button clicked for deposit btn");
+    console.log("added deposit btn for the clicked");
 })
